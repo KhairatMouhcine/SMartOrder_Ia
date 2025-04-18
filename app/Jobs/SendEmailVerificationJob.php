98ff28706ea0fe60a71906b9bfce0d7c45f097fb
KhairatMouhcine <khairatmouhcine125@gmail.com>
2025-04-18 11:00:00 +0100

    refactor: optimize application services and error handling
    Co-authored-by: Bassma02 <b.chihab2002@gmail.com>
    Co-authored-by: ELFAIZE-Youssef <youssefmae26082000@gmail.com>

---
+++ app/Jobs/SendEmailVerificationJob.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Mail\EmailVerificationMail;
+
+class SendEmailVerificationJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function handle()
+    {
+        // Envoie de l'email
+        Mail::to($this->user->email)->send(new EmailVerificationMail($this->user));
+    }
+}
